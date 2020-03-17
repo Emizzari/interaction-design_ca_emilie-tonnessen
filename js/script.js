ea957a14570032339e3287a0935ca22768ecece6
Emizzari <emilie@tonnessen.com>
2020-03-17 20:07:44 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,10 +15,79 @@ const productsDOM = document.querySelector(".products-centre");
 // cart
 let cart = [];
 
+
+
+// local storage
+class Storage {
+
+}
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const ui = new UI();
+    const products = new Products();
+
+    // get all products
+    products.getProducts().then(data => ui.displayProducts(data));
+})
+
+
+
+// End of carts and products -----------------------------------------
+
+const url = json.parse(LOCALFILE);
+
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    })
+
+    .then(function (json) {
+        productsUI(json);
+    })
+
+    .catch(function (error) {
+        console.log(error);
+    });
+
+function productsUI(json) {
+    const results = json.results;
+
+    console.log(results);
+
+    const resultsContainer = document.querySelector(".products-center");
+
+    let newHTML = "";
+
+    // Adding the new HTML code with the propper values:
+    results.forEach(function (result) {
+
+        
+        newHTML += `
+                <!-- Single product -->
+                <article class="product">
+                    <div class="img-container">
+                        <img class="product-img" src="${result.imageUrl}" alt="${result.name}">
+                        <button class="bag-btn" data-id="${result.id}">
+                            <i class="fas fa-shopping-bag"></i>
+                            add to bag
+                        </button>
+                    </div>
+                    <h3>${result.name}</h3>
+                    <h4>NOK ${result.price}</h4>
+                 </article> 
+                 <!-- End of single product -->
+            `;
+    });
+
+    resultsContainer.innerHTML = newHTML;
+}
+
+
+
 // getting the products
-class Products {
-    async getProducts(){
-        try{
+/*class Products {
+    async getProducts() {
+        try {
             let result = await fetch("shoes.json");
             let data = await result.json();
             return data;
@@ -29,15 +98,15 @@ class Products {
                 const image = item.fields.image.fields.file.url;
                 return {title,price,id,image};
             }) 
-            return products; */
+            return products; *//*
         } catch (error) {
             console.log(error);
-        }     
+        }
     }
-}
+}*/
 
 // display products
-class UI {
+/*class UI {
     displayProducts(products){
         let result = "";
 
@@ -61,21 +130,4 @@ class UI {
 
         productsDOM.innerHTML = result;
     }
-}
-
-// local storage
-class Storage {
-
-}
-
-document.addEventListener("DOMContentLoaded", ()=>{
-    const ui = new UI();
-    const products = new Products();
-
-    // get all products
-    products.getProducts().then(data => ui.displayProducts(data));
-})
-
-
-
-// End of carts and products -----------------------------------------
+}*/
